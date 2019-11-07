685608b0deac068d854b79e7a12428a01464216b
suissa <jnascimento@gmail.com>
2019-11-07 07:58:00 -0300

    jsonwebtoken

--- modules/User/controller.js
+++ modules/User/controller.js
@@ -1,43 +1,29 @@
-const Model = require('./model')
-// const ModelPF = require('./model.pf')
-// const ModelPJ = require('./model.pj')
-
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const Model = require('./model')
+
+const checkIfEmailExists = async (email) => !!(await User.find({ email }).length)
+const getPasswordHash = (pass) => bcrypt.hash(pass, 10)
+
+const comparePassword = (pass, userPass) => {
+	const password = bcrypt.compare(pass, userPass);
+	return password
+}
 
 const find = async (req, res) => await Model.find()
-// const findPF = async (req, res) => {
-//   return await ModelPF.find()
-//   // .populate('user_id')
-// }
-// const findOnePF = async (_id) => {
-//   return await ModelPF.findOne({_id})
-//   .populate('user_id')
-// }
-// const findPJ = async (req, res) => await ModelPJ.find()
-
 const create = async (data) => {
   
-  return await Model.create(data)
+  if (checkIfEmailExists(data.email)) return false
+
+  const hashedPass = getPasswordHash(data.password)
+
+  console.log({data})
+  data.password = hashedPass
+  console.log({data})
+  // return await Model.create(data)
 }
-// const createPF = async (data) => {
-  
-//   // const User = await Model.create(data)
-
-//   return await ModelPF.create(data)
-// }
-// const createPJ = async (data) => {
-  
-//   const User = await Model.create(data)
-
-//   return await ModelPJ.create(data)
-// }
 
 module.exports = {
   find,
-  // findPF,
-  // findOnePF,
-  // findPJ,
-  create,
-  // createPF,
-  // createPJ
+  create
 }
